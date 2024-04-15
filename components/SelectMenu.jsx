f48570e8ff9a9df02aa40cea7ad492b39399726d
Vikas Kumar Mishra <mishrav090@gmail.com>
2024-04-16 00:01:12 +0530

    First Time

--- components/SelectMenu.jsx
+++ components/SelectMenu.jsx
@@ -5,7 +5,7 @@ export default function SelectMenu({setQuery}) {
     <select className="filter-by-region" onChange={(e) => setQuery(e.target.value.toLowerCase())}>
       <option hidden>Filter by Region</option>
       <option value="Africa">Africa</option>
-      <option value="America">America</option>
+      <option value="Americas">Americas</option>
       <option value="Asia">Asia</option>
       <option value="Europe">Europe</option>
       <option value="Oceania">Oceania</option>
